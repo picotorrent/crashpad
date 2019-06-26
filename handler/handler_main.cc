ef17718a230eef4fee110fc32e5dd2a86a45706a
krzaq <krzaq@krzaq.cc>
2019-06-26 23:14:12 +0200

    Backtrace changeset.

--- handler/handler_main.cc
+++ handler/handler_main.cc
@@ -45,6 +45,7 @@
 #include "client/simple_string_dictionary.h"
 #include "handler/crash_report_upload_thread.h"
 #include "handler/prune_crash_reports_thread.h"
+#include "base/strings/utf_string_conversions.h"
 #include "tools/tool_support.h"
 #include "util/file/file_io.h"
 #include "util/misc/address_types.h"
@@ -165,6 +166,9 @@ void Usage(const base::FilePath& me) {
 #endif  // OS_LINUX || OS_ANDROID
 "      --url=URL               send crash reports to this Breakpad server URL,\n"
 "                              only if uploads are enabled for the database\n"
+#if defined(OS_WIN) || defined(OS_FUCHSIA) || defined(OS_LINUX)
+"      --attachment=NAME=PATH  attach a copy of a file, along with a crash dump\n"
+#endif // OS_WIN || OS_FUCHSIA || OS_LINUX
 #if defined(OS_CHROMEOS)
 "      --use-cros-crash-reporter\n"
 "                              pass crash reports to /sbin/crash_reporter\n"
@@ -189,6 +193,7 @@ void Usage(const base::FilePath& me) {
 struct Options {
   std::map<std::string, std::string> annotations;
   std::map<std::string, std::string> monitor_self_annotations;
+  std::map<std::string, base::FilePath> attachments;
   std::string url;
   base::FilePath database;
   base::FilePath metrics_dir;
@@ -244,6 +249,32 @@ bool AddKeyValueToMap(std::map<std::string, std::string>* map,
   }
   return true;
 }
+#if defined(OS_WIN) || defined(OS_FUCHSIA) || defined(OS_LINUX)
+// Overloaded version, to accept base::FilePath as a VALUE.
+bool AddKeyValueToMap(std::map<std::string, base::FilePath>* map,
+                      const std::string& key_value,
+                      const char* argument) {
+  std::string key;
+  std::string raw_value;
+  if (!SplitStringFirst(key_value, '=', &key, &raw_value)) {
+    LOG(ERROR) << argument << " requires NAME=PATH";
+    return false;
+  }
+
+#ifdef OS_WIN
+  base::FilePath value(base::UTF8ToUTF16(raw_value));
+#else
+  base::FilePath value(raw_value);
+#endif
+
+  base::FilePath old_value;
+  if (!MapInsertOrReplace(map, key, value, &old_value)) {
+    LOG(WARNING) << argument << " has duplicate name " << key
+                 << ", discarding value " << old_value.value().c_str();
+  }
+  return true;
+}
+#endif // OS_WIN || OS_FUCHSIA || OS_LINUX
 
 // Calls Metrics::HandlerLifetimeMilestone, but only on the first call. This is
 // to prevent multiple exit events from inadvertently being recorded, which
@@ -577,6 +608,9 @@ int HandlerMain(int argc,
     kOptionTraceParentWithException,
 #endif
     kOptionURL,
+#if defined(OS_WIN) || defined(OS_FUCHSIA) || defined (OS_LINUX)
+    kOptionAttachment,
+#endif // OS_WIN || OS_FUCHSIA || OS_LINUX
 #if defined(OS_CHROMEOS)
     kOptionUseCrosCrashReporter,
     kOptionMinidumpDirForTests,
@@ -656,6 +690,9 @@ int HandlerMain(int argc,
      kOptionTraceParentWithException},
 #endif  // OS_LINUX || OS_ANDROID
     {"url", required_argument, nullptr, kOptionURL},
+#if defined(OS_WIN) || defined(OS_FUCHSIA) || defined (OS_LINUX)
+    {"attachment", required_argument, nullptr, kOptionAttachment},
+#endif // OS_WIN || OS_FUCHSIA || OS_LINUX
 #if defined(OS_CHROMEOS)
     {"use-cros-crash-reporter",
       no_argument,
@@ -823,6 +860,14 @@ int HandlerMain(int argc,
         options.url = optarg;
         break;
       }
+#if defined(OS_WIN) || defined(OS_FUCHSIA) || defined(OS_LINUX)
+      case kOptionAttachment: {
+        if (!AddKeyValueToMap(&options.attachments, optarg, "--attachment")) {
+          return ExitFailure();
+        }
+        break;
+      }
+#endif // OS_WIN || OS_FUCHSIA || OS_LINUX
 #if defined(OS_CHROMEOS)
       case kOptionUseCrosCrashReporter: {
         options.use_cros_crash_reporter = true;
@@ -1013,6 +1058,10 @@ int HandlerMain(int argc,
       database.get(),
       static_cast<CrashReportUploadThread*>(upload_thread.Get()),
       &options.annotations,
+#if defined(OS_WIN) || defined(OS_FUCHSIA) || defined(OS_LINUX) || defined(OS_MACOSX)
+      // TODO(scottmg): for all platforms.
+      &options.attachments,
+#endif // OS_WIN || OS_FUCHSIA || OS_LINUX || OS_MACOSX
 #if defined(OS_FUCHSIA)
       // TODO(scottmg): Process level file attachments, and for all platforms.
       nullptr,
--- handler/linux/crash_report_exception_handler.cc
+++ handler/linux/crash_report_exception_handler.cc
@@ -62,15 +62,17 @@ CrashReportExceptionHandler::CrashReportExceptionHandler(
     CrashReportDatabase* database,
     CrashReportUploadThread* upload_thread,
     const std::map<std::string, std::string>* process_annotations,
+    const std::map<std::string, base::FilePath>* process_attachments,
     bool write_minidump_to_database,
     bool write_minidump_to_log,
     const UserStreamDataSources* user_stream_data_sources)
     : database_(database),
       upload_thread_(upload_thread),
       process_annotations_(process_annotations),
+      process_attachments_(process_attachments),
       write_minidump_to_database_(write_minidump_to_database),
       write_minidump_to_log_(write_minidump_to_log),
-      user_stream_data_sources_(user_stream_data_sources) {
+      user_stream_data_sources_(user_stream_data_sources){
   DCHECK(write_minidump_to_database_ | write_minidump_to_log_);
 }
 
@@ -188,6 +190,24 @@ bool CrashReportExceptionHandler::WriteMinidumpToDatabase(
     Metrics::ExceptionCaptureResult(
         Metrics::CaptureResult::kMinidumpWriteFailed);
     return false;
+  }
+
+  if (process_attachments_) {
+    // Note that attachments are read at this point each time rather than once
+    // so that if the contents of the file has changed it will be re-read for
+    // each upload (e.g. in the case of a log file).
+    for (const auto& it : *process_attachments_) {
+      FileWriter* writer = new_report->AddAttachment(it.first);
+      if (writer) {
+        std::string contents;
+        if (!LoggingReadEntireFile(it.second, &contents)) {
+          // Not being able to read the file isn't considered fatal, and
+          // should not prevent the report from being processed.
+          continue;
+        }
+        writer->Write(contents.data(), contents.size());
+      }
+    }
   }
 
   bool write_minidump_to_log_succeed = false;
